6c830ca0c77d2feac663ebb2a7585d7177d2f5ba
Ulan Bayaliev <ulan.besmart@gmail.com>
2015-03-19 16:06:09 +0600

    Minor change

--- stream_identify.py
+++ stream_identify.py
@@ -8,7 +8,6 @@ import time
 from glob import glob
 import fp
 
-last=0
 codegen_path = os.path.abspath("/home/ulan/echoprint-codegen/echoprint-codegen")
 
 import simplejson as json
@@ -33,43 +32,49 @@ def process_file(filename):
 	codes = codegen(filename)
 	if codes is None:
 		return -2
+	if len(codes) == 0:
+		logfile.write("Codegen returned empty list"+"\n")
+		return -3
+	if "code" not in codes[0]:
+		logfile.write("No code is returned by codegen"+"\n")
+		return -4
 
 	now_time = time.strftime('%H:%M:%S')
 	now_date = time.strftime('%Y-%m-%d')
 	db = conn.cursor()    	
 	track_id = None
 	
-    	if len(codes)>0 and "code" in codes[0]:
-        	decoded = fp.decode_code_string(codes[0]["code"])
-        	result = fp.best_match_for_query(decoded)
-        	if result.TRID:
-			track_id = result.TRID
-		else:
-			#Insert melody to unknown fingerprints table with status 'N'
-			logfile.write("No match found for the file, inserting unknown melody to fingerprint table"+'\n')
-                        db.execute("""INSERT INTO fingerprint(fp,radio,date_played,time_played,time_identified,status,radio_id,track_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",(decoded,radio,now_date,now_time,None,'N',radio_id,None))
-                        conn.commit()		
-			db.close()	
-			return -1
-		
-	global last
-	#Insert tracks only once
-	if (last == 0) or (last != track_id):
-		last=track_id
+        decoded = fp.decode_code_string(codes[0]["code"])
+        result = fp.best_match_for_query(decoded)
+        if result.TRID:
+		#Melody is recognized
+		track_id = result.TRID
+		global last
+		#Insert tracks only once
+		if (last == 0) or (last != track_id):
+			last=track_id
 
-		try:
-	   		logfile.write("Inserting track_id: " + track_id	+ " for file: " + filename+'\n')
-	   		db.execute("""INSERT INTO played_melody(track_id,radio,date_played,time_played,radio_id) VALUES (%s,%s,%s,%s,%s)""",(track_id,radio,now_date,now_time,radio_id))
-	   		conn.commit()
-		except db.Error, e:
-           		logfile.write("Error %d: %s\n" % (e.args[0],e.args[1]))
-	   		conn.rollback()
+			try:
+	   			logfile.write("Inserting track_id: " + track_id	+ " for file: " + filename+'\n')
+	   			db.execute("""INSERT INTO played_melody(track_id,radio,date_played,time_played,radio_id) VALUES (%s,%s,%s,%s,%s)""",(track_id,radio,now_date,now_time,radio_id))
+	   			conn.commit()
+			except db.Error, e:
+           			logfile.write("Error %d: %s\n" % (e.args[0],e.args[1]))
+	   			conn.rollback()
+		else:
+			logfile.write("Track is already recognized from other file "+filename+'\n')
+
+		db.close()	
+		return 0
 	else:
-		logfile.write("Track is already recognized from other file "+filename+'\n')
+		#Insert unknown fingerprints with status 'N'
+		logfile.write("No match found for the file, inserting unknown melody to fingerprint table"+'\n')
+                db.execute("""INSERT INTO fingerprint(fp,radio,date_played,time_played,time_identified,status,radio_id,track_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)""",(decoded,radio,now_date,now_time,None,'N',radio_id,None))
+                conn.commit()		
+		db.close()	
+		return -1
+		
 	
-	db.close()
-	
-	return 0
 
 if __name__ == "__main__":
  	import urllib2
@@ -80,13 +85,13 @@ if __name__ == "__main__":
                 print "Usage: python stream_identify.py radio radio_id stream"
                 exit()
 
+	last=0
         radio = sys.argv[1]
 	radio_id = sys.argv[2]
         stream = sys.argv[3]
 
    	url=urllib2.urlopen(stream)
         conn = MySQLdb.connect(host= "192.168.3.111",user="root", passwd="123", db="pymusic",charset='utf8')
-        
 
 	logfile = open("logs/radio"+radio+"logStreamIdentify"+time.strftime('%Y-%m-%d %H:%M:%S'), 'w')
         last_result = -1
