abae76bb3cf44ca122c9f1fc7439f058faf4ce2a
Ulan Bayaliev <ulan.besmart@gmail.com>
2015-04-28 12:08:25 +0600

    Reprocessing changed

--- reprocess.py
+++ reprocess.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0, "/home/ulan/echoprint-server/API")
+sys.path.insert(0, "/home/monitor/Workspace/echoprint-server/API")
 import MySQLdb
 import os
 import time    
@@ -9,16 +9,17 @@ def reprocess(fid,decoded,date_played,time_played,radio,radio_id):
 
         result = fp.best_match_for_query(decoded)
         if result.TRID:
+		logfile.write("Melody is recognized: "+result.TRID)
 		#Melody is recognized
 		track_id = result.TRID
 		current = convertTimeToMinutes(time_played)
-		global last_time,last_radio
+		global last_time,last_radio,recognized
 		try:
-			now = time.strftime('%Y-%m-%d %H:%M:%S')
-			#Insert only tracks which differ more than 4 minutes, it is do to prevent repeated insert from different parts of the same track
-			if (last_radio is None) or (last_radio != radio_id) or (last_time == 0) or (current - last_time > 4):
+			#Insert only tracks which differ more than 5 minutes, it is do to prevent repeated insert from different parts of the same track
+			if (last_radio is None) or (last_radio != radio_id) or (last_time == 0) or (current - last_time > 5):
 				last_time=current
 				last_radio=radio_id 
+				recognized = recognized + 1
 			
 				db = conn.cursor()    	
 	   			logfile.write("Inserting track_id: " + track_id	+ " for fingerprint " +str(fid)+'\n')
@@ -28,14 +29,14 @@ def reprocess(fid,decoded,date_played,time_played,radio,radio_id):
 				
 				db = conn.cursor()    	
 	   			logfile.write("Updating fingerprint " +str(fid)+ " as "+track_id +'\n')
-				db.execute("""update fingerprint set status = 'Y', track_id = %s, time_identified=%s  where id = %s""",(track_id,now,fid))
+				db.execute("""update fingerprint set status = 'Y', track_id = %s, time_identified=%s  where id = %s""",(track_id,getNowDateTime(),fid))
 	   			conn.commit()
 				db.close()
 			else:			
 				logfile.write("Track is already recognized from previous fingerprint "+radio+str(date_played)+":"+str(time_played)+'\n')
 				db = conn.cursor()    	
 	   			logfile.write("Updating fingerprint " +str(fid)+ " as Y" +'\n')
-				db.execute("""update fingerprint set status = 'Y', time_identified=%s  where id = %s""",(now,fid))
+				db.execute("""update fingerprint set status = 'Y', time_identified=%s  where id = %s""",(getNowDateTime(),fid))
 	   			conn.commit()
 				db.close()
 		
@@ -50,23 +51,36 @@ def convertTimeToMinutes(t):
 	result = int(h) * 60 + int(m)
 	return result
 
+def getNowTime():
+	return time.strftime('%H:%M:%S')
+
+def getNowDate():
+	return time.strftime('%Y-%m-%d')
+		
+def getNowDateTime():
+	return time.strftime('%Y-%m-%d %H:%M:%S')	
+
+
 if __name__ == "__main__":
-	print time.strftime('%Y-%m-%d %H:%M:%S')
+	logfile.write(getNowDateTime()+'\n')
  	if len(sys.argv) < 2:
-                print "Usage: python reprocess.py date(YYYY-mm-dd)"
+                print "Usage: python reprocess.py startDate(YYYY-mm-DD)"
                 exit()
 
         last_time=0
+	recognized = 0
 	last_radio=None
-        when = sys.argv[1]
+        startDate = sys.argv[1]
 
         conn = MySQLdb.connect(host= "localhost",user="root", passwd="ulut123", db="pymusic",charset='utf8')
 	db = conn.cursor()
 
         try:
-		logfile = open("logs/reprocess"+time.strftime('%Y-%m-%d %H:%M:%S'), 'w')
+		logfile = open("logs/reprocess"+getNowDateTime(), 'w',1)
                 
-		db.execute("""SELECT * FROM fingerprint where status='N' and date_played=%s order by radio_id,time_played""",(when,))
+		db.execute("""(SELECT * FROM fingerprint where status='N' and date_played>=%s and date_played<%s order by radio_id,time_played) union (SELECT * FROM fingerprint where status='N' and date_played=%s and time_played<%s order by radio_id,time_played)""",(startDate,getNowDate(),getNowDate(),getNowTime()))
+
+		logfile.write(db._executed+'\n')
                 # Fetch all the rows in a list of lists.
                 results = db.fetchall()
                 for row in results:
@@ -77,20 +91,11 @@ if __name__ == "__main__":
 			time_played = row[4]
 			radio_id = row[7]
 			reprocess(fid,decoded,date_played,time_played,radio,radio_id)
-			'''
-			| id              | int(11)        | NO   | PRI | NULL    | auto_increment |
-			| fp              | varchar(32000) | YES  |     | NULL    |                |
-			| radio           | varchar(255)   | YES  | MUL | NULL    |                |
-			| date_played     | date           | YES  | MUL | NULL    |                |
-			| time_played     | time           | YES  | MUL | NULL    |                |
-			| time_identified | datetime       | YES  |     | NULL    |                |
-			| status          | char(1)        | YES  |     | NULL    |                |
-			| radio_id        | int(11)        | YES  | MUL | NULL    |                |
-			| track_id        | varchar(255)   | YES  | MUL | NULL    |                |
-			'''
         except db.Error, e:
                 logfile.write("Error %d: %s" % (e.args[0],e.args[1]))
 		db.close()
+		logfile.close()
+		conn.close()	
 	except KeyboardInterrupt:
 		logfile.write("Program was interrupted using keyboard"+'\n')
         	db.close()
@@ -98,9 +103,9 @@ if __name__ == "__main__":
 		conn.close()	
 		exit()
 	
+	logfile.write(getNowDateTime()+'\n')
+	logfile.write("Songs recognized: " + str(recognized))
 	db.close()
 	logfile.close()
 	conn.close()	
-	print	time.strftime('%Y-%m-%d %H:%M:%S')
-	exit()
-
+	
