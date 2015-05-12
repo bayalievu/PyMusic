1ef4bbf9e8c8666c1dc69357e4200db97e9e5d68
Ulan Bayaliev <ulan.besmart@gmail.com>
2015-05-12 19:40:31 +0600

    Minor modification

---
+++ delete_old_fp.py
@@ -0,0 +1,42 @@
+import sys
+import MySQLdb
+import os
+import time    
+
+def getNowTime():
+	return time.strftime('%H:%M:%S')
+
+def getNowDate():
+	return time.strftime('%Y-%m-%d')
+		
+def getNowDateTime():
+	return time.strftime('%Y-%m-%d %H:%M:%S')	
+
+
+if __name__ == "__main__":
+	import datetime
+	start_date = datetime.datetime.now() + datetime.timedelta(-30)
+	logfile = open("/home/monitor/Workspace/PyMusic/logs/delete_old_fp_log", 'a',1)
+	logfile.write(getNowDateTime()+'\n')
+	logfile.write("Deleting fingerprint older than(inclusive):"+start_date.strftime('%Y-%m-%d')+'\n')
+	
+        conn = MySQLdb.connect(host= "localhost",user="root", passwd="ulut123", db="pymusic",charset='utf8')
+	cursor = conn.cursor()
+
+        try:
+                
+		cursor.execute("""delete FROM fingerprint where date_played<=%s""",(start_date.strftime('%Y-%m-%d'),))
+		conn.commit()
+		logfile.write(cursor._executed+'\n')
+        
+	except cursor.Error, e:
+                logfile.write("Error %d: %s" % (e.args[0],e.args[1]))
+		logfile.close()
+		cursor.close()
+		conn.close()
+		exit()	
+	
+	logfile.write(getNowDateTime()+'\n')
+	logfile.close()
+	cursor.close()
+	conn.close()	
